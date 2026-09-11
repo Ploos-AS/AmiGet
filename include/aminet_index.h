cec7e63815b952cd4bda61a4075306132c408bb4
pos <pgousdal@users.noreply.github.com>
2026-09-11 15:35:13 +0200

    M2: add Aminet index parser interface

---
+++ include/aminet_index.h
@@ -0,0 +1,24 @@
+#ifndef AMINET_INDEX_H
+#define AMINET_INDEX_H
+
+#include <stddef.h>
+
+#define AMINET_NAME_MAX 128
+#define AMINET_DIR_MAX 128
+#define AMINET_SIZE_MAX 32
+#define AMINET_AGE_MAX 32
+#define AMINET_DESC_MAX 256
+
+typedef struct AminetIndexEntry {
+    char name[AMINET_NAME_MAX];
+    char directory[AMINET_DIR_MAX];
+    char size[AMINET_SIZE_MAX];
+    char age[AMINET_AGE_MAX];
+    char description[AMINET_DESC_MAX];
+} AminetIndexEntry;
+
+int aminet_parse_index_line(const char *line, AminetIndexEntry *entry);
+int aminet_build_cache(const char *index_path, const char *cache_path);
+int aminet_search_cache(const char *cache_path, const char *term);
+
+#endif
