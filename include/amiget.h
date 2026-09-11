639bc5216ea18c818f1b2a040d3a9875478661cc
pos <pgousdal@users.noreply.github.com>
2026-09-11 15:21:09 +0200

    M1: add local catalogue interface

---
+++ include/amiget.h
@@ -0,0 +1,28 @@
+#ifndef AMIGET_H
+#define AMIGET_H
+
+#define AMIGET_VERSION "0.1.0-m1"
+#define AMIGET_MAX_LINE 512
+#define AMIGET_MAX_FIELD 256
+#define AMIGET_MAX_PATH 512
+
+typedef struct AmiGetPackage {
+    char format[16];
+    char name[64];
+    char version[64];
+    char summary[AMIGET_MAX_FIELD];
+    char source[32];
+    char aminet_path[AMIGET_MAX_FIELD];
+    char os_min[32];
+    char cpu_min[32];
+    char archive[32];
+    char sha256[80];
+    char install_recipe[64];
+} AmiGetPackage;
+
+int amiget_load_package(const char *path, AmiGetPackage *pkg);
+int amiget_list(const char *catalogue_path);
+int amiget_search(const char *catalogue_path, const char *term);
+int amiget_info(const char *catalogue_path, const char *name);
+
+#endif
